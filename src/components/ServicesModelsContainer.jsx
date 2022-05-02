d0d38b6749332cd81adf0a9751f373d1ebd70023
Andrea Rosales <andrearosr@gmail.com>
2022-05-02 17:30:35 -0500

    Add decorative circles

--- src/components/ServicesModelsContainer.jsx
+++ src/components/ServicesModelsContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import { Container } from './Layout';
 import Title from './Title';
+import Circle from './Circle';
 import ServiceModelCard from './ServiceModelCard';
 import data from '../static/staticServicesModelsData';
 
@@ -9,6 +10,7 @@ const SectionContainer = styled(Container)`
     position: relative;
     background: linear-gradient(180deg, rgba(5, 67, 64, 1) 62%, rgba(255,255,255,1) 62%, rgba(255,255,255,1) 71%);
     padding-top: 180px;
+    margin-bottom: 100px;
 `;
 
 const TitleContainer = styled.div`
@@ -19,6 +21,7 @@ const TitleContainer = styled.div`
 `;
 
 const CardsContainer = styled.div`
+    position: relative;
     width: 960px;
     display: flex;
     flex-wrap: wrap;
@@ -34,6 +37,18 @@ const ServicesModelsContainer = () => {
         </Title>
       </TitleContainer>
       <CardsContainer>
+        <Circle
+          color="#50706d"
+          size="450px"
+          top="-180px"
+          right="-100px"
+        />
+        <Circle
+          color="#EDF3F2"
+          size="200px"
+          bottom="-70px"
+          left="-110px"
+        />
         {data.map(item => (
           <ServiceModelCard title={item.title} text={item.text} />
         ))}
