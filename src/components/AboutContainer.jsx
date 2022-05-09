7d6c95976c997e6f46c925fd4b0b6de278e48354
Andrea Rosales <andrearosr@gmail.com>
2022-05-09 14:34:17 -0500

    Remove unused vars

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import image from '../static/VERSION-MOBIL-15.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
--- src/components/ServicesContainer.jsx
+++ src/components/ServicesContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
